13b4b3523e3718d3932f12829c6b2ae1774b9237
Sumeye <dalkilincsumeye@gmail.com>
2022-09-30 15:52:00 -0400

    Added more work

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -2,9 +2,11 @@ package com.cydeo.controller;
 
 import com.cydeo.dto.UserDTO;
 import com.cydeo.service.RoleService;
+import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,9 +14,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     private final RoleService roleService;
+    private final UserService userService;
 
-    public UserController(RoleService roleService) {
+    public UserController(RoleService roleService, UserService userService) {
         this.roleService = roleService;
+        this.userService = userService;
     }
 
     @GetMapping("/create")
@@ -24,6 +28,28 @@ public class UserController {
 
         model.addAttribute("roles",roleService.findAll());
 
+        model.addAttribute("users",userService.findAll());
+
         return "/user/create";
     }
+
+    @PostMapping ("/create")
+    public String insertUser(Model model){
+
+        //User object, roles, users
+        model.addAttribute("user", new UserDTO());
+        model.addAttribute("roles",roleService.findAll());
+        model.addAttribute("users",userService.findAll());
+
+        return "/user/create";
+    }
+
+    @PostMapping ("/create")
+    public String addUser(){
+
+        //User object, roles, users
+
+        return "/user/create";
+    }
+
 }

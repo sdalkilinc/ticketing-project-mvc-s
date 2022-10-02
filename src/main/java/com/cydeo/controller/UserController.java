d677d5b8f9552d3191947e99587e04d04f46d300
Sumeye <dalkilincsumeye@gmail.com>
2022-10-02 11:45:58 -0400

    User Controller is updated

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -5,10 +5,7 @@ import com.cydeo.service.RoleService;
 import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/user")
@@ -34,14 +31,31 @@ public class UserController {
         return "/user/create";
     }
 
-    @PostMapping ("/create")
-    public String insertUser(@ModelAttribute("user")UserDTO user){
 
+
+
+    @PostMapping("/create")
+    public String insertUser(@ModelAttribute("user") UserDTO user){
 
         userService.save(user);
 
         return "redirect:/user/create";
+
     }
+
+    @GetMapping("/update/{username}")
+    public String editUser(@PathVariable("username") String username, Model model){
+
+        model.addAttribute("user",userService.findById(username));
+
+        model.addAttribute("roles",roleService.findAll());
+
+
+        model.addAttribute("users",userService.findAll());
+
+        return "/user/update";
+    }
+
 
 
 }

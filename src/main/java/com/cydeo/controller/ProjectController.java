8016bcb7e541a46172ca18e0f38df6f6a8f65802
Sumeye <dalkilincsumeye@gmail.com>
2022-10-02 14:57:41 -0400

    More work added

---
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -0,0 +1,22 @@
+package com.cydeo.controller;
+
+import com.cydeo.dto.ProjectDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/project")
+public class ProjectController {
+
+    @GetMapping("/create")
+    public String createProject(Model model){
+
+        model.addAttribute("project",new ProjectDTO());
+
+        model.addAttribute("managers",......)
+
+        return "/project/create";
+    }
+}
